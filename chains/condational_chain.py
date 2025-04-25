c36cdedee197daa944d78a7c98a44bf8972d9a27
ekaleemarchiwiz <kaleem@archiwiz.com>
2025-04-25 19:35:57 +0500

    Refactor conditional chain to include sentiment classification and response generation for positive and negative feedback

--- chains/condational_chain.py
+++ chains/condational_chain.py
@@ -1,8 +1,9 @@
 from dotenv import load_dotenv
 from langchain.schema.output_parser import StrOutputParser
-from langchain_openai import AzureChatOpenAI, ChatOpenAI
+from langchain_openai import AzureChatOpenAI
 from langchain_core.prompts import PromptTemplate
-from langchain.schema.runnable import RunnableParallel
+from langchain.schema.runnable import RunnableBranch, RunnableLambda
+from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
 from typing import Literal
 import os
@@ -10,11 +11,47 @@ import os
 load_dotenv()
 
 # Azure OpenAI Model
-azure_model = AzureChatOpenAI(
+model = AzureChatOpenAI(
     deployment_name=os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME"),
     openai_api_version=os.getenv("AZURE_OPENAI_API_VERSION"),
     openai_api_key=os.getenv("AZURE_OPENAI_API_KEY"),
     azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
 )
 
+parser = StrOutputParser()
 
+class Feedback(BaseModel):
+    sentiment: Literal['positive', 'negative'] = Field(description='Give the sentiment of the feedback')
+
+parser2 = PydanticOutputParser(pydantic_object=Feedback)
+
+prompt1 = PromptTemplate(
+    template='Classify the sentiment of the following feedback text into positive or negative \n {feedback} \n {format_instruction}',
+    input_variables=['feedback'],
+    partial_variables={'format_instruction': parser2.get_format_instructions()}
+)
+
+classifier_chain = prompt1 | model | parser2
+
+prompt2 = PromptTemplate(
+    template='Write an appropriate response to this positive feedback: \n {feedback}',
+    input_variables=['feedback']
+)
+
+prompt3 = PromptTemplate(
+    template='Write an appropriate response to this negative feedback: \n {feedback}',
+    input_variables=['feedback']
+)
+
+# Conditional branching
+branch_chain = RunnableBranch(
+    (lambda x: x.sentiment == 'positive', prompt2 | model | parser),
+    (lambda x: x.sentiment == 'negative', prompt3 | model | parser),
+    RunnableLambda(lambda x: "Could not find sentiment"),
+)
+
+# Compose
+chain = classifier_chain | branch_chain
+
+print(chain.invoke({"feedback": "The product is worst ever"}))
+chain.get_graph().print_ascii()
